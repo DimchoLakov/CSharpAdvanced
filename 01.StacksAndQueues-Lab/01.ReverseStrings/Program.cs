920833a0f06620d9733e66d74138274f9fb0a3e0
whywherewhat <mdsektor@gmail.com>
2018-01-23 23:24:01 +0000

    Added 01.ReverseStrings

---
+++ 01.StacksAndQueues-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> charStack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                charStack.Push(c);
+            }
+
+            while (charStack.Count != 0)
+            {
+                Console.Write(charStack.Pop());
+            }
+        }
+    }
+}
