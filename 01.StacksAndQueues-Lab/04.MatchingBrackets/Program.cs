1b80863a10591eba553171bee1b8ec5adc0c4d59
whywherewhat <mdsektor@gmail.com>
2018-01-24 00:52:08 +0000

    Added 04.MatchingBrackets

---
+++ 01.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main()
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> indexesStack = new Stack<int>();
+            Stack<string> exprStack = new Stack<string>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    indexesStack.Push(i);
+                }
+                if (expression[i] == ')')
+                {
+                    int openBracketIndex = indexesStack.Peek();
+                    string miniExpr = expression.Substring(indexesStack.Pop(), i - openBracketIndex + 1);
+                    exprStack.Push(miniExpr);
+                }
+            }
+            string[] exprArray = exprStack.ToArray();
+            exprStack.Clear();
+            foreach (string str in exprArray)
+            {
+                exprStack.Push(str);
+            }
+
+            while (exprStack.Count != 0)
+            {
+                Console.WriteLine(exprStack.Pop());
+            }
+        }
+    }
+}
