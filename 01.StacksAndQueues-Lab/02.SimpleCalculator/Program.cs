08e21b7ec29e695ff83b93b57ee5aeddb1172550
whywherewhat <mdsektor@gmail.com>
2018-01-24 00:12:49 +0000

    Added 02.SimpleCalculator

---
+++ 01.StacksAndQueues-Lab/02.SimpleCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.SimpleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stack = new Stack<string>();
+
+            Array.Reverse(input);
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            while (stack.Count != 1)
+            {
+                int leftOperand = int.Parse(stack.Pop());
+                string op = stack.Pop();
+                int rightOperand = int.Parse(stack.Pop());
+                int result = 0;
+                switch (op)
+                {
+                    case "+":
+                        result = leftOperand + rightOperand;
+                        break;
+                    case "-":
+                        result = leftOperand - rightOperand;
+                        break;
+                }
+                stack.Push(result.ToString());
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
