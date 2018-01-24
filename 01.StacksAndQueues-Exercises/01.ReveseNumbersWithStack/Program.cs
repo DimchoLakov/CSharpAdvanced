6413435b20f86388eb9bf0058f1ea0c25fd1ebe5
whywherewhat <mdsektor@gmail.com>
2018-01-24 21:47:09 +0000

    Added 01.StacksAndQueues-Exercises, 01.ReverseNumbersWithStack

---
+++ 01.StacksAndQueues-Exercises/01.ReveseNumbersWithStack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ReveseNumbersWithStack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<int> numbersStack = new Stack<int>(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            while (numbersStack.Count != 0)
+            {
+                Console.Write($"{numbersStack.Pop()} ");
+            }
+        }
+    }
+}
