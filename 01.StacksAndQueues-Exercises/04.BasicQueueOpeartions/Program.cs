35effd24e5ad369bcb6c5503551d065a656f2fc6
whywherewhat <mdsektor@gmail.com>
2018-01-24 23:52:11 +0000

    Added 04.BasicQueueOperations

---
+++ 01.StacksAndQueues-Exercises/04.BasicQueueOpeartions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BasicQueueOpeartions
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int n = tokens[0];
+            int s = tokens[1];
+            int x = tokens[2];
+
+            int[] numbersToAdd = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int[] actualNumsToAdd = numbersToAdd.Take(n).ToArray();
+
+            Queue<int> queue = new Queue<int>(actualNumsToAdd);
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            int[] currentQueue = queue.ToArray();
+            if (currentQueue.Contains(x))
+            {
+                Console.WriteLine($"true");
+                return;
+            }
+
+            Console.WriteLine(currentQueue.OrderBy(e => e).FirstOrDefault());
+        }
+    }
+}
