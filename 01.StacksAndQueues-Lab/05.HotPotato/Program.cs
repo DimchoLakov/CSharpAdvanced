ab038a8a8f7716d222b0b5d81b29fa57ca4210a1
whywherewhat <mdsektor@gmail.com>
2018-01-24 21:02:55 +0000

    Added 05.HotPotato

---
+++ 01.StacksAndQueues-Lab/05.HotPotato/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.HotPotato
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            int tossTimes = int.Parse(Console.ReadLine());
+
+            while (children.Count > 1)
+            {
+                for (int i = 0; i < tossTimes - 1; i++)
+                {
+                    children.Enqueue(children.Dequeue());
+                }
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
