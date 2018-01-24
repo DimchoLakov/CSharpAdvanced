bf38bcf837de41ef3ab6c3ddd30f51b1c5de0973
whywherewhat <mdsektor@gmail.com>
2018-01-24 22:09:34 +0000

    Added 02.BasicStackOperations

---
+++ 01.StacksAndQueues-Exercises/02.BasicStackOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicStackOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int s = tokens[1];
+            int x = tokens[2];
+            
+            int[] numbersToAdd = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int[] actualNumsToAdd = numbersToAdd.Take(n).ToArray();
+
+            Stack<int> numbers = new Stack<int>(actualNumsToAdd);
+
+            for (int i = 0; i < s; i++)
+            {
+                if (numbers.Count == 0)
+                {
+                    break;
+                }
+                numbers.Pop();
+            }
+
+            int[] currentStack = numbers.ToArray();
+            if (currentStack.Contains(x))
+            {
+                Console.WriteLine($"true");
+                return;
+            }
+
+            Console.WriteLine(currentStack.OrderBy(e => e).FirstOrDefault());
+        }
+    }
+}
