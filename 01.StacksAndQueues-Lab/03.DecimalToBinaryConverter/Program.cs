3cd1b7c4329d18ca1fa095d00948e9e6723bf67d
whywherewhat <mdsektor@gmail.com>
2018-01-24 00:22:24 +0000

    Added 03.DecimalToBinaryConverter

---
+++ 01.StacksAndQueues-Lab/03.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.DecimalToBinaryConverter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int decimalNum = int.Parse(Console.ReadLine());
+
+            Stack<int> binaryStack = new Stack<int>();
+
+            if (decimalNum == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            while (decimalNum > 0)
+            {
+                int remainder = decimalNum % 2;
+                binaryStack.Push(remainder);
+                decimalNum /= 2;
+            }
+
+            while (binaryStack.Count != 0)
+            {
+                Console.Write(binaryStack.Pop());
+            }
+        }
+    }
+}
