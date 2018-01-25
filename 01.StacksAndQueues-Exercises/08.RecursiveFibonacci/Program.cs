eb68cefd7f68194c1b11a5b13caa3d253eb6e1f2
whywherewhat <mdsektor@gmail.com>
2018-01-25 23:21:14 +0000

    Added 08.RecursiveFibonacci

---
+++ 01.StacksAndQueues-Exercises/08.RecursiveFibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08.RecursiveFibonacci
+{
+    class Program
+    {
+        private static long[] memoFibNumbers;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            memoFibNumbers = new long[n + 1];
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        static long GetFibonacci(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            if (memoFibNumbers[n] == 0)
+            {
+                memoFibNumbers[n] = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+
+            return memoFibNumbers[n];
+        }
+    }
+}
