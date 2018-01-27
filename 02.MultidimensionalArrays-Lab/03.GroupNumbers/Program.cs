cf67bd0f6d9d50cdae69dce370d386475aada1f3
whywherewhat <mdsektor@gmail.com>
2018-01-27 22:36:53 +0000

    Added 03.GroupNumbers

---
+++ 02.MultidimensionalArrays-Lab/03.GroupNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GroupNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse).ToArray();
+
+            int[][] jaggedArray = new int[3][];
+
+            int[] sizes = new int[3];
+
+            foreach (int number in numbers)
+            {
+                sizes[Math.Abs(number % 3)]++;
+            }
+
+            for (int index = 0; index < jaggedArray.Length; index++)
+            {
+                int arrLength = sizes[index];
+                jaggedArray[index] = new int[arrLength];
+            }
+
+            int[] indexes = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                int remainder = Math.Abs(currentNumber % 3);
+                jaggedArray[remainder][indexes[remainder]] = currentNumber;
+                indexes[remainder]++;
+            }
+
+            foreach (int[] ints in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", ints));
+            }
+        }
+    }
+}
