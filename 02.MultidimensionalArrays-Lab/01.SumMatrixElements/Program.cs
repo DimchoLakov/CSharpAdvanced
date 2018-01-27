7697b77dd0aa4a154ae1d1cdfb9e4f984f7fad50
whywherewhat <mdsektor@gmail.com>
2018-01-27 01:24:15 +0000

    Added 02.MultidimensionalArrays - Lab, 01.SumMatrixElements

---
+++ 02.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            int rows = matrixSize[0];
+            int columns = matrixSize[1];
+
+            int[,] matrix = new int[rows, columns];
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int[] matrixElements = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = matrixElements[j];
+                    sum += matrix[i, j];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(columns);
+            Console.WriteLine(sum);
+        }
+    }
+}
