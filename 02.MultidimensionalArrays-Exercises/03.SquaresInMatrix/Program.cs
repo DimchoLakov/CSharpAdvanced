db5e20ff7d01f912818f04f8d9760d3d8a2530db
whywherewhat <mdsektor@gmail.com>
2018-01-28 20:20:22 +0000

    Added 03.SquaresInMatrix

---
+++ 02.MultidimensionalArrays-Exercises/03.SquaresInMatrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace _03.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] sizeMatrix = Console.ReadLine()
+                            .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int rowsLength = sizeMatrix[0];
+            int columnsLength = sizeMatrix[1];
+
+            
+            int[,] matrix = new int[rowsLength, columnsLength];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] letters = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = letters[col];
+                    //Console.WriteLine($"[{row}, {col}] = {(char)matrix[row, col]}");
+                }
+            }
+
+            int matricesFound = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int topLeft = matrix[row, col];
+                    int topRight = matrix[row, col + 1];
+                    int botLeft = matrix[row + 1, col];
+                    int botRight = matrix[row + 1, col + 1];
+
+                    if (topLeft == topRight && topLeft == botLeft && topLeft == botRight)
+                    {
+                        matricesFound++;
+                    }
+                }
+            }
+
+            Console.WriteLine(matricesFound);
+        }
+    }
+}
