523138b1a92ad71194230e71ea1a4b4140db76a4
whywherewhat <mdsektor@gmail.com>
2018-02-05 18:48:38 +0000

    Added 03.Custom Min Function

---
+++ 04.FuntionalProgramming-Exercises/03.CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> getMinNumber = x =>
+            {
+                int minNumber = int.MaxValue;
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (minNumber > x[i])
+                    {
+                        minNumber = x[i];
+                    }
+                }
+                return minNumber;
+            };
+
+            Console.WriteLine(getMinNumber(numbers));
+        }
+    }
+}
