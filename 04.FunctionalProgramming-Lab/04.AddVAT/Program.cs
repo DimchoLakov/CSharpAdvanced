4f99ed9ab658359e2c90109f52b1b8fdc5c872dd
whywherewhat <mdsektor@gmail.com>
2018-02-05 16:57:33 +0000

    Added 04.Add VAT

---
+++ 04.FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, double> parseDouble = x => double.Parse(x);
+            Func<double, double> addVAT = x => x += x * 0.2;
+
+            List<double> numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parseDouble)
+                .Select(addVAT)
+                .ToList();
+
+            numbers.ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+
+        //static double DoubleParser(string numberAsString)
+        //{
+        //    return double.Parse(numberAsString);
+        //}
+        //
+        //static double AddVAT(double number)
+        //{
+        //    return number + number * 0.2;
+        //}
+    }
+}
