830bfd01c433223f3d724794b28fc051c824ef5d
whywherewhat <mdsektor@gmail.com>
2018-02-05 16:35:50 +0000

    Added new Solution to 01.Sort Even Numbers

--- 04.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
+++ 04.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -7,11 +7,23 @@ namespace _01.SortEvenNumbers
     {
         static void Main()
         {
-            Console.WriteLine(string.Join(", ", Console.ReadLine()
-                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
+            //Console.WriteLine(string.Join(", ", Console.ReadLine()
+            //    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .OrderBy(x => x)
+            //    .Where(x => x % 2 == 0).ToList()));
+
+            Func<string, int> intParser = int.Parse;
+            Func<int, bool> isNumberEven = n => n % 2 == 0;
+
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(intParser)
+                .Where(isNumberEven)
                 .OrderBy(x => x)
-                .Where(x => x % 2 == 0).ToList()));
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }
---
+++ 04.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int numbersCount = numbers.Length;
+            int sum = numbers.Sum();
+
+            Console.WriteLine(numbersCount);
+            Console.WriteLine(sum);
+        }
+    }
+}
