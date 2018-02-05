bb4484095697c35c1c0d163c5469d3b90cd74bc1
whywherewhat <mdsektor@gmail.com>
2018-02-05 16:45:55 +0000

    Added 03.Count Uppercase Words

---
+++ 04.FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+
+            Func<string, bool> checkIsFirstLetterCapital = str => char.IsUpper(str[0]);
+
+            string[] words = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Where(checkIsFirstLetterCapital).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
