6ddd568dd9b159da9f1cb8947d5750c17a54848c
whywherewhat <mdsektor@gmail.com>
2018-02-05 20:48:36 +0000

    Added 05.Applied Arithmetics

---
+++ 04.FuntionalProgramming-Exercises/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] boundaries = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int lowerBound = boundaries[0];
+            int upperBound = boundaries[1];
+            List<int> numbers = new List<int>(upperBound - lowerBound + 1);
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                numbers.Add(i);
+            }
+            string oddOrEven = Console.ReadLine();
+
+            Func<int, bool> isEven = x => x % 2 == 0;
+            Func<int, bool> isOdd = x => x % 2 != 0;
+
+            if (oddOrEven == "even")
+            {
+                numbers = numbers.Where(isEven).ToList();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            else if (oddOrEven == "odd")
+            {
+                numbers = numbers.Where(isOdd).ToList();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
---
+++ 04.FuntionalProgramming-Exercises/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Func<int[], int[]> add = x => x.Select(n => n += 1).ToArray();
+            Func<int[], int[]> subtract = x => x.Select(n => n -= 1).ToArray();
+            Func<int[], int[]> multiply = x => x.Select(n => n *= 2).ToArray();
+            Action<int[]> printNumbers = x => Console.WriteLine(string.Join(" ", x));
+
+            string command = Console.ReadLine();
+
+            while (!command.Equals("end"))
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = add(numbers);
+                        break;
+                    case "subtract":
+                        numbers = subtract(numbers);
+                        break;
+                    case "multiply":
+                        numbers = multiply(numbers);
+                        break;
+                    case "print":
+                        printNumbers(numbers);
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
