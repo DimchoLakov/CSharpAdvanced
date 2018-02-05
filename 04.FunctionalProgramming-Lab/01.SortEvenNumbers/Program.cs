0a0d25e340b9badd918a6d484d012ebcdc66e83b
whywherewhat <mdsektor@gmail.com>
2018-02-05 16:21:37 +0000

    Added 01.Sort Even Numbers 04.Functional Programming - Lab

---
+++ 04.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Where(x => x % 2 == 0).ToList()));
+        }
+    }
+}
