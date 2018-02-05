d8d1b7a93824e4b34d80d91bafc03a238244e4e9
whywherewhat <mdsektor@gmail.com>
2018-02-05 18:41:53 +0000

    Added 02.Knights of Honor

---
+++ 04.FuntionalProgramming-Exercises/01.ActionPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _01.ActionPoint
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> printNames = x => Console.WriteLine(string.Join(Environment.NewLine, names));
+            printNames(names);
+        }
+    }
+}
---
+++ 04.FuntionalProgramming-Exercises/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            
+            Action<string> printName = x => Console.WriteLine(string.Format($"Sir {x}"));
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                printName(names[i]);
+            }
+        }
+    }
+}
