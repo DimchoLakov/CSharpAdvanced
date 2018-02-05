2301b8cf37626dbd28810d87b8b840f783e7642b
whywherewhat <mdsektor@gmail.com>
2018-02-05 21:18:22 +0000

    Added 07.PredicateForNames

---
+++ 04.FuntionalProgramming-Exercises/07.PredicateForNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isShortEnough = x => x.Length <= n;
+
+            foreach (string name in names)
+            {
+                if (isShortEnough(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
