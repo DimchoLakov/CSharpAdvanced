bc9d7e4ded230b43568419d0de234b01d0ef4d60
whywherewhat <mdsektor@gmail.com>
2018-02-05 21:14:02 +0000

    Added 06.Reverse and Exclude

---
+++ 04.FuntionalProgramming-Exercises/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+            numbers.RemoveAll(num => num % n == 0);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
