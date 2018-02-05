40d6e0631ca7785612d8513a59bd5a30e65836a2
whywherewhat <mdsektor@gmail.com>
2018-02-05 16:40:31 +0000

    Solution to Problem 02. Sum Numbers Refactored

--- 04.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
+++ 04.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -7,15 +7,21 @@ namespace _02.SumNumbers
     {
         static void Main()
         {
+            Func<string, int> intParser = ParseInt;
             int[] numbers = Console.ReadLine()
                 .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray();
+                .Select(ParseInt).ToArray();
 
             int numbersCount = numbers.Length;
             int sum = numbers.Sum();
 
             Console.WriteLine(numbersCount);
             Console.WriteLine(sum);
+        }
+
+        static int ParseInt(string numberAsString)
+        {
+            return int.Parse(numberAsString);
         }
     }
 }
