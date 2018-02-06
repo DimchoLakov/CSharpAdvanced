23c53fad606879214e6e29bd597f1a4e0c730870
whywherewhat <mdsektor@gmail.com>
2018-02-06 17:26:52 +0000

    Added 13.Tri Function

---
+++ 04.FuntionalProgramming-Exercises/11.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ 04.FuntionalProgramming-Exercises/13.TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13.TriFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Func<string, int, bool> filter = (name, sum) => name.ToCharArray().Sum(x => x) >= sum;
+
+            string result = names.FirstOrDefault(name => filter(name, n));
+            Console.WriteLine(result);
+        }
+    }
+}
